1c84ba522dc4681dbae827d5dd93c8cbd354c8a8
Gerry Agbobada <gagbobada+git@gmail.com>
2017-12-20 00:14:18 +0100

    Add a few modes and general purpose settings

--- init.el
+++ init.el
@@ -28,6 +28,11 @@
   "Load a file in current user's configuration directory"
   (load-file (expand-file-name file user-init-dir)))
 
+(savehist-mode 1)
+(setq inhibit-startup-message t)
+(setq-default indent-tabs-mode nil)
+
+;; Appearance related stuff
 (load-user-file "appearance.el")
 
 ;; Load Helm
@@ -38,17 +43,46 @@
 
 ;; Load magit
 (use-package magit
+  :ensure t
+  :config
+  (use-package evil-magit
+    :ensure t))
+
+(use-package projectile
   :ensure t)
+
+(use-package clang-format
+  :ensure t
+  :config
+  (global-set-key [F10] 'clang-format-region)
+  )
+
+(use-package cc-mode
+  :ensure t)
+
+(use-package elpy
+  :ensure t
+  :config
+  (elpy-enable)
+  (setq elpy-rpc-python-command "python3")
+  (setq elpy-rpc-backend "jedi")
+  (elpy-use-cpython "/usr/bin/python3")
+  (setq python-check-command "/usr/bin/pyflakes-3")
+  (add-hook 'python-mode-hook (
+            lambda () (show-paren-mode 1))
+            )
+  )
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (helm evil-mode use-package evil-visual-mark-mode))))
+ '(package-selected-packages (quote use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(setq c-default-style "k&r")
