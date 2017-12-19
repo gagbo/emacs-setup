2c3d0d3f1db62db1435056895259933c85dd387a
Gerry Agbobada <gagbobada+git@gmail.com>
2017-12-20 00:13:55 +0100

    Use helm properly for fuzzy matching

--- helm.el
+++ helm.el
@@ -1,16 +1,21 @@
 (use-package helm
   :ensure t
-  :config
-  ; rebind tab to run persistent action
-  (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
-  ; make TAB work in terminal
-  (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
-  ; list actions using C-z
-  (define-key helm-map (kbd "C-z")  'helm-select-action)
-
+  :init
   (setq helm-autoresize-max-height 0)
   (setq helm-autoresize-min-height 20)
+  :config
+  (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+  (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
+  (define-key helm-map (kbd "C-z")  'helm-select-action)
   (helm-autoresize-mode 1)
+  (define-key helm-map (kbd "C-j") 'helm-next-line)
+  (define-key helm-map (kbd "C-k") 'helm-previous-line)
+  (define-key helm-map (kbd "C-h") 'helm-next-source)
+  (define-key helm-map (kbd "C-S-h") 'describe-key)
+  (define-key helm-map (kbd "C-l") (kbd "RET"))
+  (define-key helm-map [escape] 'helm-keyboard-quit)
+  (define-key evil-ex-map "b " 'helm-mini)
+  (define-key evil-ex-map "e " 'helm-find-files)
 
   (helm-mode 1)
   )
