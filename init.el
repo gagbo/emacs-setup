b120be2ff35e02fb6406a31529167a58315ff5b7
Gerry Agbobada <gagbobada+git@gmail.com>
2017-12-19 19:34:18 +0100

    Initial commit

---
+++ init.el
@@ -0,0 +1,66 @@
+(require 'package)
+
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+(use-package evil
+  :ensure t
+  :config
+  (use-package evil-leader
+    :ensure t
+    :config
+    (evil-leader/set-leader "<SPC>")
+    (global-evil-leader-mode))
+
+  (use-package evil-jumper
+    :ensure t
+    :config
+    (global-evil-jumper-mode))
+
+  (use-package evil-surround
+    :ensure t
+    :config
+    (global-evil-surround-mode))
+
+  (use-package evil-indent-textobject
+    :ensure t)
+
+  (use-package evil-magit
+    :ensure t)
+
+  (use-package evil-matchit
+    :ensure t)
+
+  (use-package evil-nerd-commenter
+    :ensure t)
+
+  (evil-mode 1)
+  )
+(use-package helm
+  :ensure t)
+(use-package magit
+  :ensure t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (helm evil-mode use-package evil-visual-mark-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
