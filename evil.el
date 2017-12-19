09881385fa355e47da54896665c00791fd4cac21
Gerry Agbobada <gagbobada+git@gmail.com>
2017-12-20 00:13:43 +0100

    Define evil mappings

--- evil.el
+++ evil.el
@@ -1,28 +1,21 @@
-(defun minibuffer-keyboard-quit ()
-  "Abort recursive edit.
-  In Delete Selection mode, if the mark is active, just deactivate it;
-  then it takes a second \\[keyboard-quit] to abort the minibuffer."
-  (interactive)
-  (if (and delete-selection-mode transient-mark-mode mark-active)
-    (setq deactivate-mark  t)
-    (when (get-buffer "*Completions*") (delete-windows-on "*Completions*"))
-    (abort-recursive-edit)))
-
-(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
-(global-set-key [escape] 'evil-exit-emacs-state)
-
 (use-package evil
   :ensure t
   :config
+  (evil-mode 1)
+
   (use-package evil-leader
     :ensure t
     :config
+    (global-evil-leader-mode)
     (evil-leader/set-leader "<SPC>")
-    (global-evil-leader-mode))
+    (evil-leader/set-key
+      "e" 'helm-find-file
+      "bd" 'kill-buffer-and-window
+      "gs" 'magit-status
+      "ne" 'flycheck-next-error
+      "pe" 'flycheck-previous-error
+      )
+    )
 
   (use-package evil-surround
     :ensure t
@@ -41,7 +34,20 @@
   (use-package evil-nerd-commenter
     :ensure t)
 
+  (use-package evil-commentary
+    :ensure t)
+
+  (use-package evil-lion
+    :ensure t)
+
+  (use-package evil-tutor
+    :ensure t)
+
+  (use-package evil-visual-mark-mode
+    :ensure t)
+
   (define-key evil-normal-state-map [escape] 'keyboard-quit)
   (define-key evil-visual-state-map [escape] 'keyboard-quit)
-  (evil-mode 1)
+  (define-key evil-normal-state-map (kbd ";") 'evil-ex)
+  (define-key evil-normal-state-map (kbd ":") 'evil-repeat-find-char)
   )
