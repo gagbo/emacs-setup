d0ef4b78f7fc90d727979377ceee2d7163849084
Gerry Agbobada <gagbobada+git@gmail.com>
2017-12-21 09:43:32 +0100

    Add SML for OSSU

--- init.el
+++ init.el
@@ -121,6 +121,9 @@
             )
   )
 
+(use-package sml-mode
+  :ensure t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
