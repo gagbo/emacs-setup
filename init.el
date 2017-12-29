3d866d301932334768f0591b3c1df12e09635cff
Gerry Agbobada <gagbobada+git@gmail.com>
2017-12-29 14:15:23 +0100

    Change the order of loading to prevent the security prompt of iceberg

--- init.el
+++ init.el
@@ -14,9 +14,6 @@
          user-init-directory)
         (t "~/.emacs.d/")))
 
-(org-babel-load-file (expand-file-name "configuration.org" user-init-dir))
-
-;;; init.el ends here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -24,10 +21,14 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("5fca5b6ff3e36e90644411d59035e42bf5fe67b31fcce91ea023816373c0e87c" default))))
+    ("ceee73442c3e53ca08d0f91800bda4e6867fa372a9e8019a8fb022988ae4bcd3" "5fca5b6ff3e36e90644411d59035e42bf5fe67b31fcce91ea023816373c0e87c" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(org-babel-load-file (expand-file-name "configuration.org" user-init-dir))
+
+;;; init.el ends here
